e55dff7cba424597d68308dbfda306b26610f8e3
md.rubel <md.rubel@HOAP2406101764A>
2021-12-03 23:57:51 +0600

    Add User Class and Interface

---
+++ BuilderDesignPattern.Demo/Interface/IUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuilderDesignPattern.Demo.Interface
+{
+    public interface IUser
+    {
+        string Name { get; }
+        string Address { get; }
+        int Age { get; }
+    }
+}
